57d7dc7de83d682d403f0e2782681f7968549e3b
rubensojunior <rubensojunior6@gmail.com>
2020-07-01 23:38:12 -0300

    implementing create user test

--- src/modules/users/services/CreateUserService.spec.ts
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -1,3 +1,18 @@
-test('sum two numbers', () => {
-  expect(1 + 2).toBe(3)
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository'
+import CreateUserService from './CreateUserService'
+
+describe('CreateUser', () => {
+  it('should be able to create a new user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository()
+    const createUser = new CreateUserService(fakeUsersRepository)
+
+    const user = await createUser.execute({
+      name: 'Joseph Monkey',
+      email: 'josephmonkey@gmail.com',
+      type: 'common',
+      password: '12345678',
+    })
+
+    expect(user).toHaveProperty('id')
+  })
 })
