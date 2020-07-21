28f764edc0e87f9f747f434b06e698984b79189b
Fernando José <fernando.jose.santos@hotmail.com>
2020-07-21 13:27:59 -0300

    migrations create meansures

---
+++ src/shared/infra/typeorm/migrations/1595286264596-CreateMeansures.ts
@@ -0,0 +1,8 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+export default class CreateMeansures1595286264596
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {}
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
