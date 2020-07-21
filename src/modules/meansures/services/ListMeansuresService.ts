8d3f4343a7009932f6bff697a1e045e498e7251f
Fernando José <fernando.jose.santos@hotmail.com>
2020-07-21 20:08:17 -0300

    Alter meansures router

--- src/modules/meansures/services/ListMeansuresService.ts
+++ src/modules/meansures/services/ListMeansuresService.ts
@@ -2,6 +2,9 @@ import Meansure from '@modules/meansures/infra/typeorm/entities/Meansure'
 import { injectable, inject } from 'tsyringe'
 import IMeansuresRepository from '../repositories/IMeansuresRepository'
 
+interface IRequest {
+  id: string
+}
 @injectable()
 class ListMeansuresService {
   constructor(
@@ -9,7 +12,7 @@ class ListMeansuresService {
     private meansuresRepository: IMeansuresRepository,
   ) {}
 
-  public async execute(): Promise<Meansure[]> {
+  public async execute({ id }: IRequest): Promise<Meansure[]> {
     const meansures = await this.meansuresRepository.findAllById(id)
 
     return meansures
