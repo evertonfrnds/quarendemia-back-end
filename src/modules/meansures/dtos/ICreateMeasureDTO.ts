dd0d2630aafab644811b107db227d6e7bc523b78
Fernando José <fernando.jose.santos@hotmail.com>
2020-07-23 09:16:17 -0300

    Adicionando o DTO de Measure

---
+++ src/modules/meansures/dtos/ICreateMeasureDTO.ts
@@ -0,0 +1,21 @@
+export default interface ICreateMeasureDTO {
+  id: string
+  client_id: string
+  height: number
+  weight: number
+  neck: number
+  torax_top: number
+  torax_bottom: number
+  bust: number
+  waist: number
+  abdomen: number
+  hip: number
+  thigh_left: number
+  thigh_right: number
+  calf_left: number
+  calf_right: number
+  arm_left: number
+  arm_right: number
+  forearm_left: number
+  forearm_right: number
+}
